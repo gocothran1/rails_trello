5a0bc0c37b85b92395ab0360ee669b4652328210
George Cothran <gocothran1@gmail.com>
2019-11-14 14:57:15 -0500

    You can now add cards to the database

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,12 @@
 class CardsController < ApplicationController
+    def create
+        @list = List.find(params[:list_id])
+        @card = @list.cards.create(list_id: params[:list_id], content: params[:content])
+        redirect_to home_index_path
+    end
+
+    private
+        def card_params
+            params.require(:content)
+        end
 end
